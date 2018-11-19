4671e8ee23e073a1d2fd3dcb06a8f940401a939b
Eugene <deus.sles@gmail.com>
2018-11-19 18:42:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+(function() {
+    $('.first').on('click', function(){
+        $('.down1').toggle();
+    });
+    $('.second').on('click', function(){
+        $('.down2').toggle();
+
+    });
+    $('.trird').on('click', function(){
+        $('.down3').toggle();
+
+    });
+    $('.four').on('click', function(){
+        $('.down4').toggle();
+
+    });
+    $('.fift').on('click', function(){
+        $('.down5').toggle();
+
+    });
+
+}
+)()
+
